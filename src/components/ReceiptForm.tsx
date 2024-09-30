3c77f0f116574d7db6f47911f71bcdfb9c75584f
Gino Luraschi <Gino.Luraschi@plantandfood.co.nz>
2024-09-30 15:09:04 +1300

    feat: Add styles to buttons

--- src/components/ReceiptForm.tsx
+++ src/components/ReceiptForm.tsx
@@ -149,7 +149,7 @@ const ReceiptForm = () => {
                 <InputText name="name" value={receiptToSave.name} onChange={onChange} type='text' />
             </div>
 
-            <button type="button" onClick={() => setIsOpen(true)} className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="button" onClick={() => setIsOpen(true)} className="text-white bg-primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary">
                 Elija una foto
             </button>
             <IonModal isOpen={isOpen}>
@@ -191,7 +191,7 @@ const ReceiptForm = () => {
             <br />
             <StepsTable steps={steps} setSteps={setSteps} />
             <br />
-            <button type="submit" className="bg-primary bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{params.id ? "Actualizar" : "Crear"}</button>
+            <button type="submit" className="bg-primary bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary">{params.id ? "Actualizar" : "Crear"}</button>
         </form>
     );
 };
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,6 +3,7 @@ import './Register.css'
 import { useState } from "react"
 import { createUserWithEmailAndPass, getCurrentUser } from "../firebase/auth"
 import { useHistory } from "react-router"
+import InputText from "../components/inputText"
 
 const RegisterPage: React.FC = () => {
     const [user, setUser] = useState({
@@ -33,38 +34,26 @@ const RegisterPage: React.FC = () => {
     return <IonPage>
         <IonHeader translucent>
             <IonToolbar>
-                Cree un usuario para sus guardar recetas
+                Create your user
             </IonToolbar>
         </IonHeader>
         <IonContent>
-            <form className="max-w-md mx-auto form" onSubmit={e => createUser(e)}>
-                <div className="relative z-0 w-full mb-5 group">
-                    <input type="email" name="userName" id="floating_email"
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        value={user.userName} onChange={e => onChange(e)}
-                        placeholder=" " required />
-                    <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
+            <form className="max-w-md mx-auto form flex flex-col items-center w-full" onSubmit={e => createUser(e)}>
+                <div className="relative z-0 w-full">
+                    <InputText value={user.userName} type="email" onChange={onChange} placeholder="Email" name="userName" />
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="password" id="floating_password"
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        value={user.password} onChange={e => onChange(e)}
-                        placeholder=" " required />
-                    <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Contrasenia</label>
+                <div className="relative z-0 w-full">
+                    <InputText value={user.password} type="password" onChange={onChange} placeholder="Password" name="password" />
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                    <input type="password" name="secondPassword" id="floating_repeat_password"
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        value={user.secondPassword} onChange={e => onChange(e)}
-                        placeholder=" " required />
-                    <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirmar contrasena</label>
+                <div className="relative z-0 w-full">
+                    <InputText value={user.secondPassword} type="password" onChange={onChange} placeholder="Repeat password" name="secondPassword" />
                 </div>
                 <button type="button"
                     onClick={() => { history.push("/login") }}
                     className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                    Login con usuario existente
+                    Go to Login
                 </button>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear</button>
+                <button type="submit" className="text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary">Create</button>
             </form>
         </IonContent>
     </IonPage>
